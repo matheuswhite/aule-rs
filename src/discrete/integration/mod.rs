720adecf028515098cceff860b426076edf57436
Matheus T. dos Santos <matheuswhite@citrinio.com>
2025-07-31 21:36:56 -0300

    docs: add documentation for integration/mod.rs

--- src/discrete/integration/mod.rs
+++ src/discrete/integration/mod.rs
@@ -4,6 +4,21 @@ pub mod euler;
 pub mod runge_kutta;
 
 pub trait Discretizable<T> {
+    /// Converts a continuous-time system into a discrete-time system.
+    ///
+    /// # Returns
+    /// A new system that implements the `Integrator` trait.
+    ///
+    /// # Type Parameters
+    /// `T`: The type of the discrete-time system.
+    ///
+    /// # Example
+    /// ```
+    /// use aule::prelude::*;
+    ///
+    /// let tf = 1.0 / (s - 1.0);
+    /// let discrete_tf: Euler = tf.discretize();
+    /// ```
     fn discretize(self) -> T
     where
         T: Integrator;
